de1620bfb59e27ad71a824c445d69495f4e0e12c
pushpamgroupmobile <team.it@pushpamgroup.in>
2017-04-19 17:30:28 +0530

    old google sharing method added

--- SocialMediaShareToTimeLine/AppDelegate.swift
+++ SocialMediaShareToTimeLine/AppDelegate.swift
@@ -7,40 +7,93 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import GoogleSignIn
+import Google
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate {
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FBSDKApplicationDelegate .sharedInstance() .application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        FBSDKLoginButton.superclass()
+        FBSDKProfilePictureView.superclass()
+        
+        
+        
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
+        
+        // GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().clientID = "599280904951-9usr5as4773rkimklle3o6a08q3tui4i.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().serverClientID = "599280904951-il34rh0tagk0f8406bdltiei6ioh4fsa.apps.googleusercontent.com"
+        
+        GIDSignIn.sharedInstance().shouldFetchBasicProfile =  true
+        GIDSignIn.sharedInstance().delegate = self
+        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let result = GIDSignIn.sharedInstance().handle(url,
+                                                       //added exclamation mark
+            sourceApplication: String(describing: options[UIApplicationOpenURLOptionsKey.sourceApplication]!),
+            annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        
+        
+        return result
+        
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        print("on app delegate didSignInFor ")
+        
+        if (error == nil) {
+            
+            print("user Data is fetched")
+            print(user.userID)
+            
+        } else {
+            print("\(error.localizedDescription)")
+        }
+        
+    }
+    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user:GIDGoogleUser!,
+                withError error: NSError!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+    }
+    
+    
+    
 }
 
--- SocialMediaShareToTimeLine/ViewController.swift
+++ SocialMediaShareToTimeLine/ViewController.swift
@@ -7,19 +7,178 @@
 //
 
 import UIKit
+import Social
+import SafariServices
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FBSDKShareKit
 
-class ViewController: UIViewController {
+import GoogleSignIn
+import Google
 
+
+class ViewController: UIViewController, SFSafariViewControllerDelegate, FBSDKSharingDelegate {
+    
+    
+    let lacartImageView:UIImageView = UIImageView()
+    
+    //native
+    @IBOutlet var facebookShareButton: UIButton!
+    @IBOutlet var googlePlusShareButton: UIButton!
+    
+    //From SDK
+    @IBOutlet var facebookShare: FBSDKShareButton!
+    
+    var content : FBSDKShareLinkContent = FBSDKShareLinkContent()
+    var fbDetails : NSDictionary?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lacartImageView.image = UIImage(named: "chef_icon")
+        
+        
+        //From SDK
+        content.contentURL = NSURL(string: "http://www.lacart.com/") as URL!
+        content.contentTitle = "Good News!"
+        content.contentDescription = "Now experience better, healthier & fresher meals, made at a home near you. Enter your exact location to explore. Made with love by food architects around you..\n"
+        content.imageURL = NSURL(string: "chef_icon") as URL!
+        
+        facebookShare.shareContent = content
+        facebookShare.isEnabled =  true
+        verifyInitialAccessToFacebook()
     }
+    func verifyInitialAccessToFacebook(){
+        
+        if FBSDKAccessToken.current() != nil {
+            FBSDKLoginManager().logOut()
+            return
+        }
+        
+        let loginManager:FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if (error == nil){
+                let fbloginresult : FBSDKLoginManagerLoginResult = result!
+                if fbloginresult.grantedPermissions != nil {
+                    if(fbloginresult.grantedPermissions.contains("email"))
+                    {
+                        self.getFacebookUserData()
+                        loginManager.logOut()
+                    }
+                }
+            }
+        }
+    }
+    func getFacebookUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, email, picture.type(large)"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.fbDetails = result as? NSDictionary
+                    // print(self.fbDetails!)
+                    
+                 //   self.usernameLabel.text = self.fbDetails?.object(forKey: "name") as! String?
+                   // self.passwordLabel.text = self.fbDetails?.object(forKey: "email") as! String?
+                  
+                    let profilePictureDict =  self.fbDetails?.object(forKey: "picture") as? NSDictionary
+                    let profilePictureDataDict =  profilePictureDict?.object(forKey: "data") as? NSDictionary
+                    print("pictDataArray  :\(profilePictureDataDict )")
+                  //  self.profilePictureImageURL.text = "Image URL : "+(profilePictureDataDict?.object(forKey: "url") as! String?)!
+                    
+                }else{
+                    print(error?.localizedDescription ?? "Not found")
+                }
+            })
+        }
+    }
+    
+    
+    @IBAction func shareToFacebookButtonClicked(_ sender: Any) {
+        
+        let vc = SLComposeViewController(forServiceType:SLServiceTypeFacebook)
+        vc?.add(lacartImageView.image!)
+        vc?.add(URL(string: "http://www.lacart.com/"))
+        vc?.setInitialText("Good News! Now experience better, healthier & fresher meals, made at a home near you. Enter your exact location to explore. Made with love by food architects around you..\n")
+        self.present(vc!, animated: true, completion: nil)
+    }
+    
+    @IBAction func shareToGoogleButtonClicked(_ sender: Any) {
+        let urlstring = "https://developers.google.com/+/mobile/ios/share/basic-share"
+        let shareURL = NSURL(string: urlstring)
+        let urlComponents = NSURLComponents(string: "https://plus.google.com/share")
+        urlComponents!.queryItems = [NSURLQueryItem(name: "url", value: shareURL!.absoluteString) as URLQueryItem]
+        let url = urlComponents!.url!
+        if #available(iOS 9.0, *) {
+            let svc = SFSafariViewController(url: url)
+            svc.delegate = self
+            svc.title = "Lacart"
+            self.present(svc, animated: true, completion: nil)
+        } else {
+            debugPrint("Not available")
+        }
+    }
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController){
+        
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool)
+    {
+        
+        
+    }
+    
+    @IBAction func facebookShareClicked(_ sender: Any) {
+        
+        
+        
+        print("sahr e clicked")
+        
+        let shareDialog = FBSDKMessageDialog()
+        shareDialog.shareContent = content
+        // shareDialog.completion = { result in
+        // Handle share results
+        //   }
+        
+        shareDialog.show()
+    }
+    @IBAction func googleShareClicked(_ sender: Any) {
+        
+        //This form is deprecated
 
+//        var activityItems: [Any]? = ["Hello Google+!", UIImage(named: "example.jpg")!]
+//
+//        activityItems = ["Hello Google+!", URL(string: "https://github.com/lysannschlegel/GooglePlusShareActivity")]
+//        
+//        // You can also set up a GPPShareBuilder on your own. All other items will be ignored
+//        
+//        
+//        var shareBuilder:  GPPNativeShareBuilder? = (GPPShare.sharedInstance().nativeShareDialog as? GPPNativeShareBuilder)
+//        shareBuilder?.prefillText = "Hello Google+!"
+//        
+//        shareBuilder?.URLToShare = URL(string: "https://github.com/lysannschlegel/GooglePlusShareActivity")
+//        activityItems = ["Does not appear", shareBuilder]
+//       
+//        var gppShareActivity = GPPShareActivity()
+//        gppShareActivity.canShowEmptyForm = true
+    }
+    func sharer(_ sharer: FBSDKSharing!, didCompleteWithResults results: [AnyHashable : Any]!){
+        
+    }
+    
+    func sharer(_ sharer: FBSDKSharing!, didFailWithError error: Error!){
+        
+    }
+    func sharerDidCancel(_ sharer: FBSDKSharing!){
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
